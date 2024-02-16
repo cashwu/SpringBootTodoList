85dfe3740ade2058bddea6bbc8982a74c92ddc9b
cash <cashwugeek@icloud.com>
2024-02-16 10:50:50 +0800

    modify view model

--- src/main/java/com/cashwu/todolist/controller/AppController.java
+++ src/main/java/com/cashwu/todolist/controller/AppController.java
@@ -1,5 +1,6 @@
 package com.cashwu.todolist.controller;
 
+import com.cashwu.todolist.entity.Todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,11 @@ public class AppController {
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("helloKey", "Hello World ~");
+        Todo todo = new Todo();
+        todo.setId(1);
+        todo.setTask("tt");
+        todo.setStatus(123);
+        model.addAttribute("todo", todo);
         return "hello";
     }
 
