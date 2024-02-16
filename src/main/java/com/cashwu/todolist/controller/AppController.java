a76d3a488644d2b5f04811152466ab59c0b1f3b0
cash <cashwugeek@icloud.com>
2024-02-16 11:45:32 +0800

    add post redirect to page

--- src/main/java/com/cashwu/todolist/controller/AppController.java
+++ src/main/java/com/cashwu/todolist/controller/AppController.java
@@ -4,14 +4,17 @@ import com.cashwu.todolist.entity.Todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 /**
  * @author cash
  */
 @Controller
 public class AppController {
-
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("helloKey", "Hello World ~");
         Todo todo = new Todo();
@@ -19,7 +22,24 @@ public class AppController {
         todo.setTask("tt");
         todo.setStatus(123);
         model.addAttribute("todo", todo);
+
+        model.addAttribute("gender", "female");
+
+        var list = new ArrayList<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        model.addAttribute("list", list);
+
         return "hello";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute Todo todo, Model model) {
+        model.addAttribute("todo", todo);
+        return "add";
     }
 
 }
