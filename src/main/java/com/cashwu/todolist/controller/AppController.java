89a1a6354635459f4ca7b1e06029d0a5243422a7
cash <cashwugeek@icloud.com>
2024-02-16 10:32:08 +0800

    add view model

--- src/main/java/com/cashwu/todolist/controller/AppController.java
+++ src/main/java/com/cashwu/todolist/controller/AppController.java
@@ -1,6 +1,7 @@
 package com.cashwu.todolist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AppController {
 
     @GetMapping("hello")
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("helloKey", "Hello World ~");
         return "hello";
     }
 
